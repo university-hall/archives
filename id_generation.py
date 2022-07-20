9785f46f32797e21538c759e768a19a4840e17e0
Artorivs <arthurcheunglw@icloud.com>
2022-07-21 01:07:42 +0800

    Update datasets

--- id_generation.py
+++ id_generation.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from archivepy.GenerateID import generate_uuid
 
-archives = pd.read_csv('./data/1_archives.csv', dtype={'year': str})
-archives['year'] = pd.to_datetime(archives['year'], format='%Y') # assign the datatype to datetime
+archives = pd.read_csv('archives/data/101_suggestion_book.csv', 
+                       dtype={'year': str})
+
+archives['start_date'] = pd.to_datetime(archives['start_date'], format='%Y-%m-%d')
+archives['end_date'] = pd.to_datetime(archives['end_date'], format='%Y-%m-%d')
+
+archives['year'] = archives['end_date'].dt.year
+
+archives['year'] = pd.to_datetime(archives['year'], 
+                                  format='%Y') # assign the datatype to datetime
 
 archives = generate_uuid(archives)
 
-archives.to_csv('./data/1_archives.csv', index=False)
+archives.to_csv('archives/data/101_suggestion_book.csv', index=False)
